2be0e143a8026bafd83bcdc068b3b1e1bf8630ee
mertsoker <mrtsoker@gmail.com>
2025-03-22 14:20:35 +0300

    ReservationRepository.java

--- ReservationRepository.java
+++ ReservationRepository.java
@@ -4,6 +4,10 @@ import com.example.libraryplace1.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
+
+import java.util.List;
+@Repository  
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findByStudents_Id(Long studentId);
 }
